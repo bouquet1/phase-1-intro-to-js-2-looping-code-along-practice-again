53ebd89cfd4774d0b7179e4315de9c50cd0d3bbd
bouquet1 <bukuleta@gmail.com>
2023-04-01 15:42:11 -0700

    completed

--- index.js
+++ index.js
@@ -1,59 +1,37 @@
-// Code your solutions in this file
-// for (let age = 30; age < 40; age++){
-//     console.log(`I'm ${age} years old. Happy birthday to me!`);
-//     debugger;
+// The for loop is made up of four statements in the following structure:
+// for([initialization]; [condition]; [iteration]) {
+    //[loop body]
 // }
-
-// const gifts = ["teddy bear", "drone", "doll"];
-
-// function wrapGifts(gifts) {
-//     for (let i = 0; i < gifts.length; i++) {
-//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//         debugger;
-//     }
-//     return gifts;
-// }
-
-// wrapGifts(gifts);
-
-// const name = ["Charlie", "Samip", "Ali"];
-
-// function writeCards(name, eventName) {
-//     for (let i = 0; i < name.length; i++) {
-//         console.log(`Thank you, ${name}, for the wonderful ${eventName} gift!`);
-//         debugger;
-//     }
-//     return name;
-// }
+// Usage: Use a for loop when you know how many times you want the loop to run (for example, when you're looping through elements in an array).
 
 
-const names =["Guadalupe", "Ollie","Aki"];
+const names = ["Guadalupe", "Ollie", "Aki"];
 const messages = [];
 
-// function writeCards(names, eventNames){    
-//     for (let i = 0; i < names.length; i ++) { 
-//     const message = `Thank you, ${names[i]}, for the wonderful {eventNames} gift!`;    
-//     message.push(message);
-//     } 
-//     return message;
-// }
-
-function writeCards(names, eventNames){
-    for (let i = 0; i < names.length; i ++){
-    let message =  `Thank you, ${names[i]}, for the wonderful ${eventNames} gift!`;
+function writeCards(names, occasion){
+    for (let i = 0; i < names.length; i ++) {
+    let message =  `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
     messages.push(message);
     }   
     return messages;
 }
 
+// I tried to write  it in a new way but it didn't work :(
+// const writeCArds = (names, occasion) => {
+//     for (let i = 0; i < names.length; i ++) {
+//         let message =  `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+//         messages.push(message);
+//         }   
+//     return messages; 
+// }
 
-function countDown(number){   
-    while (number >= 0) {       
-        console.log(number);       
-        number --;   
+function countDown(number){
+    while (number >= 0) {
+        console.log(number--);
     }
 }
 
-
-
-
+// THIS ONE WORKS TOO!
+// for (let countDown = 0; countDown , 10; countDown--) {
+//     console.log(countDown)
+// }
